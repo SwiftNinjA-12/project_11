8d33629b6229f9f091fac69204187486b30dce8a
Tom Verveckken <tverveckken@gmail.com>
2022-10-11 11:58:36 +0200

    end of vid 361

--- app.js
+++ app.js
@@ -5,35 +5,33 @@ const express = require("express");
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'))// where it will find the template files
+app.set('view engine', 'ejs')//method to set certain options. 'view engine' is to tell it to look at the template. ejs is the template type
+
 app.use(express.static('public'));//to be able to use static sites ie css + JS files
 app.use(express.urlencoded({ extended: false }));// to parse incoming data
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+//   const htmlFilePath = path.join(__dirname, "views", "index.html");
+//   res.sendFile(htmlFilePath); this gets replaced with res.render('index') as it is now going to render the template file, and it already knows where the template files are.
+res.render('index');
 });
 
 app.get("/about", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath);
+    res.render('about');
 });
 
 app.get("/confirm", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath);
+    res.render('confirm');
 });
 
 app.get("/index", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+    res.render('index');
 });
 
 app.get("/recommend", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlFilePath);
+    res.render('recommend');
 });
-
-
 
 app.post("/recommend", function (req, res) {                            //.post is for form submission
     const restaurant = req.body                                         //stores the body of the form overall. instead of req.body.name or req.body.website or other inputs. creates an object 
@@ -45,12 +43,8 @@ app.post("/recommend", function (req, res) {                            //.post 
     res.redirect('/confirm')                                            //once data is submitted go to confirm page
 });
 
-
-
-
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath);
+    res.render('restaurants');
 });
 
 app.listen(3000);
